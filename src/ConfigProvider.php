dbc3142901698fd71f7399265117560eb045b201
呼吸二氧化碳 <jonwang@queyang.com>
2019-09-06 14:31:47 +0800

    增加 BASE_DIR 的定义判断

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -8,9 +8,12 @@ use Hyperf\Contract\ConfigInterface;
 use Hyperf\Contract\StdoutLoggerInterface;
 use Hyperf\Framework\Bootstrap\WorkerStartCallback;
 
-class ConfigProvider
-{
+class ConfigProvider {
     public function __invoke(): array {
+        if (!defined('\\BASE_DIR')) {
+            define('\\BASE_DIR', (defined('\\BASE_PATH') ? BASE_PATH : dirname(__DIR__, 4)) .'/');
+        }
+
         return [
             'dependencies' => [
                 ConfigInterface::class        => ConfigFactory::class,
