bb0dea6ed5e78e426cd35f807128d84276e36921
呼吸二氧化碳 <jonwang@queyang.com>
2019-10-09 18:47:25 +0800

    增加更多的命令行参数支持

--- src/Command/StartServer.php
+++ src/Command/StartServer.php
@@ -12,6 +12,7 @@ use Psr\Container\ContainerInterface;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -24,12 +25,65 @@ class StartServer extends SymfonyCommand
      */
     private $container;
 
-    public function __construct(ContainerInterface $container)
-    {
+    public function __construct(ContainerInterface $container) {
         parent::__construct('start');
         $this->container = $container;
+    }
 
+    protected function configure() {
         $this->setDescription('Start the server.');
+        $this->addOption('daemon', 'd', InputOption::VALUE_NONE, 'daemonize server process');
+        $this->addOption('log', 'l', InputOption::VALUE_OPTIONAL, 'log file path');
+        $this->addOption('worker-num', 'w', InputOption::VALUE_OPTIONAL, 'worker process number');
+        $this->addOption('pid-file', null, InputOption::VALUE_OPTIONAL, 'pid file');
+        $this->addOption('host', null, InputOption::VALUE_OPTIONAL, 'main server hostname', '0.0.0.0');
+        $this->addOption('port', 'p', InputOption::VALUE_OPTIONAL, 'main server port');
+        $this->addOption('group', 'group', InputOption::VALUE_OPTIONAL, 'change group');
+        $this->addOption('user', 'user', InputOption::VALUE_OPTIONAL, 'change user');
+    }
+
+    protected function parseOption(InputInterface $input, & $config) {
+        $log = $input->getOption('log');
+        if ($log) {
+            $config['log']['path'] = $log;
+        }
+
+        $daemon = $input->getOption('daemon');
+        if ($daemon) {
+            $config['swoole']['daemonize'] = 1;
+        }
+
+        $user = $input->getOption('user');
+        if ($user) {
+            $config['swoole']['user'] = $user;
+        }
+
+        $group = $input->getOption('group');
+        if ($group) {
+            $config['swoole']['user'] = $group;
+        }
+
+        $pidFile = $input->getOption('pid-file');
+        if ($pidFile) {
+            $config['swoole']['pid_file'] = $pidFile;
+        }
+
+        $host = $input->getOption('host');
+        if ($host) {
+            $mainKey                           = key($config['hosts']);
+            $config['hosts'][$mainKey]['host'] = $host;
+        }
+
+        $port = intval($input->getOption('port'));
+        if ($port > 0) {
+            $mainKey                           = key($config['hosts']);
+            $config['hosts'][$mainKey]['port'] = $port;
+        }
+
+        $workerNum = intval($input->getOption('worker-num'));
+        if ($workerNum > 0) {
+            $config['swoole']['worker_num'] = $workerNum;
+        }
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
@@ -51,6 +105,8 @@ class StartServer extends SymfonyCommand
             throw new \InvalidArgumentException('At least one server should be defined.');
         }
         $myqeeConfig = $configFactory->getMyQEEConfig();
+
+        $this->parseOption($input, $myqeeConfig);
 
         # 创建 MyQEE 服务器
         $serverFactory->createMyQEEServer($myqeeConfig);
