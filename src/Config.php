994a77ea134e89293992d5ef87b4833b7ebe1443
呼吸二氧化碳 <jonwang@queyang.com>
2019-10-11 20:25:49 +0800

    优化 redis 配置的同步

--- src/Config.php
+++ src/Config.php
@@ -132,8 +132,65 @@ class Config implements ConfigInterface {
             }
         }
 
+        // 将 myqee redis 同步到 hyperf redis
+        $tmp = & $this->configs['redis'];
+        foreach ($MyQEEConfig['redis'] as $k => $v) {
+            $opt = $v['options'] ?? [];
+            if (isset($v['prefix'])) {
+                $opt[\Redis::OPT_PREFIX] = $v['prefix'];
+            }
+
+            if (!isset($tmp[$k])) {
+                $tmp[$k] = [
+                    'host'    => $v['host'] ?? '127.0.0.1',
+                    'auth'    => $v['auth'] ?? '',
+                    'port'    => $v['port'] ?? 6379,
+                    'pool'    => [
+                        'min_connections' => 1,
+                        'max_connections' => 10,
+                        'connect_timeout' => 10.0,
+                        'wait_timeout'    => 3.0,
+                        'heartbeat'       => -1,
+                        'max_idle_time'   => (float)env('REDIS_MAX_IDLE_TIME', 60),
+                    ],
+                    'options' => $opt,
+                ];
+            }
+            else {
+                $tmp[$k]['host'] = $v['host'] ?? '127.0.0.1';
+                $tmp[$k]['auth'] = $v['auth'] ?? '';
+                $tmp[$k]['port'] = $v['port'] ?? 6379;
+                foreach ($opt as $kk => $vv) {
+                    // 不可以用 array_merge, 否则会导致key错误
+                    $tmp[$k]['options'][$kk] = $vv;
+                }
+            }
+        }
+        unset($tmp);
+
+        $tmp =& $MyQEEConfig['redis'];
+        foreach ($this->configs['redis'] as $k => $v) {
+            $opt = $v['options'] ?? [];
+            if (!isset($tmp[$k])) {
+                $tmp[$k] = [
+                    'host'    => $v['host'] ?? '127.0.0.1',
+                    'auth'    => $v['auth'] ?? '',
+                    'port'    => $v['port'] ?? 6379,
+                    'options' => $opt,
+                ];
+            }
+            else {
+                $tmp[$k]['host'] = $v['host'] ?? '127.0.0.1';
+                $tmp[$k]['auth'] = $v['auth'] ?? '';
+                $tmp[$k]['port'] = $v['port'] ?? 6379;
+                foreach ($opt as $kk => $vv) {
+                    $tmp[$k]['options'][$kk] = $vv;
+                }
+            }
+        }
+        unset($tmp);
+
         $MyQEEConfig['servers']  = $servers;
-        $MyQEEConfig['redis']    = $this->configs['redis'] = array_merge($this->configs['redis'] ?? [], $MyQEEConfig['redis'] ?? []);
         $MyQEEConfig['swoole']   = $hyServerConfig['settings'] = array_merge($hyServerConfig['settings'], $MyQEEConfig['swoole'] ?? []);
         $this->configs['server'] = $hyServerConfig;
         $this->configs['myqee']  = $MyQEEConfig;
